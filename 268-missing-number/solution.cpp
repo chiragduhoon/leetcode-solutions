b2c8d882176616e62b06b41697a20a4cb0333980
Chirag Duhoon <chiragduhoon1@gmail.com>
2026-06-30 23:46:23 +0530

    Add solution: 268. Missing Number (Easy)

---
+++ 268-missing-number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       
+    int n=nums.size();
+    int s=n*(n+1)/2;
+    int sum=0;
+    for(int i=0 ; i<nums.size(); i++){
+        sum=sum+nums[i];
+    }
+    return s-sum;
+        
+    }
+};
