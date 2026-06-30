d8da490fe443a1f6eac7ad5e6bb21bde0481f517
Chirag Duhoon <chiragduhoon1@gmail.com>
2026-06-30 23:45:11 +0530

    Add solution: 151. Reverse Words in a String (Medium)

---
+++ 151-reverse-words-in-a-string/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans;
+        int i=s.size()-1;
+        while(i>=0){
+            while(  i>=0  &&  s[i]==' ' ){
+                i--;
+            }
+            if(i < 0) break;
+            int j=i;
+            while(i>=0 && s[i]!=' ')
+                i--;
+                if(ans.size()>0){
+                    ans=ans+' ';
+                }
+                ans=ans+s.substr(i+1,j-i);
+            
+        }
+        return ans;
+    }
+};
