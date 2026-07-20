0c55e38cba0a147b901b5dd82c439e11bd2571f6
Chirag Duhoon <chiragduhoon1@gmail.com>
2026-07-20 23:13:57 +0530

    Add solution: 645. Set Mismatch (Easy)

---
+++ 645-set-mismatch/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>freq(n+1,0);
+        vector<int>ans(2,0);
+        for(int x : nums){
+            freq[x]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(freq[i]==2){
+                ans[0]=i;
+            };
+            if(freq[i]==0){
+                ans[1]=i;
+            }
+        }
+        return ans;
+    }
+};
